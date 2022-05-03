48c01b1131b94b6e3c93b62130af2767266a43a4
Minvydas <mimciks@gmail.com>
2022-05-04 01:41:02 +0300

    update

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,9 +1,37 @@
 function App() {
   return (
-    <div className="App">
-      {" "}
-      <p>asdasd</p>
-    </div>
+    <body id="home">
+      <link
+        rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.6.1/css/all.css"
+        integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP"
+        crossOrigin="anonymous"
+      ></link>
+      <header className="hero">
+        <div id="navbar" className="navbar top">
+          <h1 className="logo">
+            <img
+              src={process.env.PUBLIC_URL + "/images/pcbuildstrans.jpg"}
+              alt="PcBuilds"
+            ></img>
+          </h1>
+        </div>
+
+        <div className="content">
+          <h1>PC Builds</h1>
+          <p>Susitiksime greitai!</p>
+        </div>
+      </header>
+
+      <footer className="footer bg-dark">
+        <div className="social">
+          <a href="https://www.facebook.com/groups/1835508580061169">
+            <i className="i fab fa-facebook fa-2x"></i>
+          </a>
+        </div>
+        <p>Copyright &copy; 2022 - PC Builds Lietuva</p>
+      </footer>
+    </body>
   );
 }
 
